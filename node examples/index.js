adbb0b8192555d624372f93bffe096702f4c50db
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-06 16:09:35 +0530

    rectangle example

---
+++ node examples/index.js
@@ -0,0 +1,21 @@
+var rect = {
+    perimeter : (x, y) => { return (2*(x+y)) },
+    area: (x, y) => { return (x*y) }
+}
+
+function solveRect(x, y) {
+    console.log(`solving for x:y = ${x}:${y}`);
+    if (x <=0 || y <=0) {
+        console.log("the sides must be greater than zero");
+        return
+    }
+    else{
+        console.log(`perimeter is ${rect.perimeter(x, y)}`);
+        console.log(`area is ${rect.area(x, y)}`);        
+    }
+}
+
+solveRect(2, 5);
+solveRect(9,5);
+solveRect(0, 8);
+solveRect(-6, -8);
