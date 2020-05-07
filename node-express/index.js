49e9aba4a4d64b8e6a3774a6b8d3e93eab3298bf
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-07 10:02:45 +0530

    node express API example

--- node-express/index.js
+++ node-express/index.js
@@ -15,8 +15,39 @@ app.listen(port, hostname, () =>{
 })
 
 app.use( (req, res, next) =>{
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>hello world!</h1><p>i'm from express server</p>")
+    res.setHeader("X-Powered-By", "krypton")
+    next();
 })
+
+//#region dishes API
+
+app.get("/dishes",(req, res)=>{
+    res.end("sending you all the dishes");
+})
+
+app.get("/dishes/:id",(req, res)=>{
+    res.end(`sending you the details of dish with id ${req.params.id}`);
+})
+
+app.post("/dishes",(req, res)=>{
+    res.end("new dish added");
+})
+
+app.put("/dishes",(req, res)=>{
+    res.end("operation not supported");
+})
+
+app.put("/dishes/:id",(req, res)=>{
+    res.end(`updating details of dish with id ${req.params.id}`);
+})
+
+app.delete("/dishes",(req, res)=>{
+    res.end("deleting all the dishes");
+})
+
+app.delete("/dishes/:id",(req, res)=>{
+    res.end(`deleting the details of dish with id ${req.params.id}`);
+})
+
